4f80299996fbc8eea7964b632818941301f9edca
teleos <liangbo80g@gmail.com>
2019-04-27 19:10:57 +0800

    steam基础学习

---
+++ src/main/java/cn/liang/learnjava8/stream/Trader.java
@@ -0,0 +1,56 @@
+package cn.liang.learnjava8.stream;
+
+public class Trader {
+    private final String name;
+    private final String city;
+
+    public Trader(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "Trade{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
+
+class Transaction{
+    private final Trader trader;
+    private final int year;
+    private final int value;
+
+    public Transaction(Trader trader, int year, int value) {
+        this.trader = trader;
+        this.year = year;
+        this.value = value;
+    }
+
+    public Trader getTrader() {
+        return trader;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    public String toString(){
+        return "{" + this.trader + ", " +
+                "year: "+this.year+", " +
+                "value:" + this.value +"}";
+    }
+}
